b2a02d9915c041196c0fbddbb482646101c1a967
Julian Bühler <juli27@users.noreply.github.com>
2017-10-31 14:47:41 +0100

    enforce utf-8 encoding

--- src/de/uulm/in/vs/grn/a1/NumberGuessingGameServer.java
+++ src/de/uulm/in/vs/grn/a1/NumberGuessingGameServer.java
@@ -5,66 +5,71 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.concurrent.ThreadLocalRandom;
 
-public class NumberGuessingGameServer {
+public final class NumberGuessingGameServer {
 
   private static final int SERVER_PORT = 5555;
+  private static final Charset charset = StandardCharsets.UTF_8;
 
   public static void main(String[] main) {
     try (ServerSocket serverSocket = new ServerSocket(SERVER_PORT)) {
       while (!serverSocket.isClosed()) {
         Socket connection = serverSocket.accept();
 
-        // don't crash the server when an exception occurs with the client connection
-        // and auto close the socket
-        try {
-          InputStream in = connection.getInputStream();
-          OutputStream out = connection.getOutputStream();
+        System.out.println("[" + connection.getInetAddress().getHostAddress() +"] connected");
 
+        // don't crash the server when an exception occurs with the client connection.
+        // closing one of the streams closes the socket
+        try (InputStream in = connection.getInputStream();
+            OutputStream out = connection.getOutputStream()) {
           out.write(("\r\nWelcome to Luisa's secret NumberGuessing game!\r\n"
-              + "I'll grant you 6 turns to guess my secret number between 0 and 50\r\n").getBytes());
+              + "I'll grant you 6 turns to guess my secret number between 0 and 50\r\n").getBytes(
+              charset));
 
           boolean won = false;
           int number = ThreadLocalRandom.current().nextInt(50);
 
-          for(int attemptsLeft = 6; attemptsLeft > 0; --attemptsLeft) {
-            byte[] buffer = new byte[8];
-            int len = in.read(buffer);
-            String input = new String(buffer, 0, len).trim();
+          for (int attemptsLeft = 6; attemptsLeft > 0; --attemptsLeft) {
+            out.write("> ".getBytes(charset));
 
+            byte[] buffer = new byte[32];
+            int len = in.read(buffer);
+            String line = new String(buffer, 0, len).trim();
             int guess;
 
             try {
-              guess = Integer.parseInt(input);
+              guess = Integer.parseInt(line);
             } catch (NumberFormatException ex) {
-              out.write("That's not a valid number!\r\n".getBytes());
+              out.write("That's not a valid number!\r\n".getBytes(charset));
               ++attemptsLeft;
               continue;
             }
 
             if (guess < number) {
-              out.write("Your guess is too low.".getBytes());
+              out.write("Your guess is too low.".getBytes(charset));
             } else if (guess > number) {
-              out.write("Your guess is too high.".getBytes());
+              out.write("Your guess is too high.".getBytes(charset));
             } else {
               won = true;
               break;
             }
 
-            out.write((" Remaining turns: " + (attemptsLeft - 1) + "\r\n").getBytes());
+            out.write((" Remaining turns: " + (attemptsLeft - 1) + "\r\n").getBytes(charset));
           }
 
           if (won) {
-            out.write("Congratulations, you won!\r\n".getBytes());
+            out.write("Congratulations, you won!\r\n\r\n".getBytes(charset));
           } else {
-            out.write("You lost :(\r\n".getBytes());
+            out.write("You lost :(\r\n\r\n".getBytes(charset));
           }
         } catch (IOException | RuntimeException ex) {
           ex.printStackTrace();
         }
 
-        connection.close();
+        System.out.println("[" + connection.getInetAddress().getHostAddress() +"] disconnected");
       }
     } catch (IOException ex) {
       ex.printStackTrace();
